87e405546bfb7e7e73be4518e42387c993271403
Itayi Paul Patrick Chitovoro <patrick@chitosystems.com>
2015-02-06 11:55:49 +0200

    config.php changes

--- _config.php
+++ _config.php
@@ -1,4 +1,9 @@
 <?php
 
+/**
+ *
+ */
+
 define('UTILITY_DIR', basename(dirname(__FILE__)));
-define('BOOTSTRAP_DIR', UTILITY_DIR . "/bower_components/bootstrap/dist/");
+define('UTILITY_BOWER_DIR', UTILITY_DIR . "/bower_components/");
+define('BOOTSTRAP_DIR', UTILITY_BOWER_DIR . "bootstrap/dist/");
