7cd29c6d791fe35c058c7683969ee674942b6e7b
Itayi Paul Patrick Chitovoro <patrick@chitosystems.com>
2015-02-06 11:01:09 +0200

    init commit

---
+++ _config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('UTILITY_DIR', basename(dirname(__FILE__)));
+define('BOOTSTRAP_DIR', UTILITY_DIR . "/bootstrap");
+define('CHOSEN_DIR', UTILITY_DIR . "/chosen");
+define('DATA_TABLES_DIR', UTILITY_DIR . "/datatables");
+define('ISOTOPE_DIR', UTILITY_DIR . "/isotope");
+define('RATY_DIR', UTILITY_DIR . "/raty");
+define('TINYMCE_DIR', UTILITY_DIR . "/tinymce");
+define('PLUGINS_DIR', UTILITY_DIR . "/plugins");
+
