8edc567623719fab2ecde01047c41e9a4147a3bc
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2015-04-29 11:54:53 +0200

    batch code update -

--- _config.php
+++ _config.php
@@ -9,11 +9,11 @@ define('PLUGINS_DIR', UTILITY_DIR . "/plugins");
 define('UTILITY_BOWER_DIR', UTILITY_DIR . "/bower_components/");
 define('BOOTSTRAP_DIR', UTILITY_BOWER_DIR . "bootstrap/dist");
 define('JQUERY_DIR', UTILITY_BOWER_DIR . "jquery/dist");
-define('JQUERY_MIGRATE_DIR', UTILITY_BOWER_DIR . "jquery-migrate/");
-define('JQUERY_FORM_DIR', UTILITY_BOWER_DIR . "jquery-form/");
-define('UNDERSCORE_DIR', UTILITY_BOWER_DIR . "underscore/");
+define('JQUERY_MIGRATE_DIR', UTILITY_BOWER_DIR . "jquery-migrate");
+define('JQUERY_FORM_DIR', UTILITY_BOWER_DIR . "jquery-form");
+define('UNDERSCORE_DIR', UTILITY_BOWER_DIR . "underscore");
 define('CHOSEN_PLUGIN_DIR', UTILITY_DIR . "/chosen");
 define('REVOLUTION_SLIDER_DIR', UTILITY_DIR . "/rs-plugin");
-define('MOMENT_DIR', UTILITY_BOWER_DIR . "/moment");
-define('CHOSEN_DIR', UTILITY_BOWER_DIR . "/chosen");
+define('MOMENT_DIR', UTILITY_BOWER_DIR . "moment");
+define('CHOSEN_DIR', UTILITY_BOWER_DIR . "chosen");
 
